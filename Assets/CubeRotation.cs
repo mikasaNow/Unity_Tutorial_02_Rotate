2ffff185d4dabded3ac6ecc296f240380a9a4c42
mikasa <mikasanow@gmail.com>
2015-04-14 19:44:51 +0900

    first
    初回コミット
    回転練習用プロジェクト

---
+++ Assets/CubeRotation.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeRotation : MonoBehaviour {
+
+	public float x = 0.0f;
+	public float y = 0.0f;
+	public float z = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		// 初期位置はプロパティ（transform.rotation）に設定
+		// Quaternion.Eulerでオイラー角をクォータニオンに変換して設定
+		//transform.rotation = Quaternion.Euler(0, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// 相対的に回転させる場合はtransform.Rotateを使用
+		transform.Rotate(x, y, z);
+
+
+		//transform.rotation = Quaternion.FromToRotation(transform.up, transform.forward );
+	}
+}
